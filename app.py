9ad1931e0d7886cbd6e0567a1277befe100f1b4f
yuta <katayama@xshell.io>
2018-06-29 18:13:52 +0900

    Use stdout

--- app.py
+++ app.py
@@ -10,12 +10,5 @@ def get_temp():
 
 
 if __name__ == '__main__':
-    while True:
-        try:
-            requests.post("http://localhost:8880/deliver",
-			      params={"soracom": "harvest"},
-			      json={"temperature": get_temp()})
-        except:
-            print(sys.exc_info())
-
-        time.sleep(60)
+        print('{"humidity": {}}'.format(get_temp()))
+        time.sleep(30)
