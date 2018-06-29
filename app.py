bfbce76726eda62a9f5536b0459812b1b4b4f230
yuta <katayama@xshell.io>
2018-06-29 19:08:50 +0900

    Script testing

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import random
 import time
 import sys
 import json
+import os
 
 
 def get_temp():
     return random.randint(0, 50)
 
 
+print(os.environ['SOME_API'])
 if __name__ == '__main__':
         print(json.dumps({'humidity': get_temp()}))
         time.sleep(30)
